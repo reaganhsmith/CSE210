0886ad82a6a7dfcf375c7b112d1376767a29188b
reaganhsmith <reagansmith@byui.edu>
2023-04-17 13:47:25 -0600

    prep3 loops

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,62 +4,21 @@ class Program
 {
     static void Main(string[] args)
         {
-    //     Console.WriteLine("Hello Prep1 World!");    
 
-    //     Console.Write("What is your first name? ");
-    //     string fname = Console.ReadLine();
+            // HOW TO GET USER INPUT
+        Console.WriteLine("Hello Prep1 World!");    
 
-    //     Console.Write("What is your last name? ");
-    //     string lname = Console.ReadLine();
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
 
-    //     Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
 
 
 
 
-        Console.WriteLine("What is you grade percentage? ");
-        string grade = Console.ReadLine();
-        int gradeNumber = int.Parse(grade);
-
-        string gradeSecond = grade.Substring(1);
-        int gradeSecNum = int.Parse(gradeSecond);
-
-        string letter = " "; 
-        string symbol = " ";
-
-        if (gradeSecNum >= 7){
-            symbol = "+";
-        }
-        else{
-            symbol = "-";
-        }
-        if (gradeNumber >= 94)
-        {
-            letter = "A"; 
-            Console.WriteLine($"Congrats you have an {letter}, you will pass the class!");
-        }
-        else if (gradeNumber <= 93)
-        {
-            letter = "A"; 
-            Console.WriteLine($"Congrats you have an {letter}{symbol}, you will pass the class!");
-        }
-        else if (gradeNumber >= 80){
-            letter = "B"; 
-            Console.WriteLine($"Congrats you have an {letter}{symbol}, you will pass the class!");  
-        }
-        else if (gradeNumber >= 70){
-            letter = "A"; 
-            Console.WriteLine($"Congrats you have an {letter}{symbol}, you will pass the class!");    
-        }
-        else if (gradeNumber >= 60){
-            letter = "A"; 
-            Console.WriteLine("You have a D, you will NOT pass the class. Better luck next time");    
-        }
-        else
-        {
-            letter = "A"; 
-             Console.WriteLine("You have a F, you will NOT pass the class. Better luck next time"); 
-        }
 
         
     }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine(" ");
+
+        // WHILE AND FOR LOOPS 
+        Random randomGenerator = new Random();
+
+        int magic = randomGenerator.Next(1, 100);
+        int userGuess = -1;
+
+
+        while (userGuess != magic)
+        {
+
+        
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        userGuess = int.Parse(guess);
+
+        if (magic > userGuess){
+            Console.WriteLine("Higher");
+        }
+        else if (magic == userGuess) {
+            Console.WriteLine("You guessed it!");
+        }
+        else{
+            Console.WriteLine("Lower");
+        }
+
+        }
     }
 }
