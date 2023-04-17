0db53c665fafcd39c7efe3313297f2f3320f7ad5
reaganhsmith <reagansmith@byui.edu>
2023-04-17 14:01:30 -0600

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine(" ");
+
+        //Creating lists 
+
+        List<int> numbers = numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, hit 0 when done");
+        int userInput = 1;
+
+        while (userInput != 0){
+            Console.Write("Enter Number: ");
+            string input = Console.ReadLine();
+            userInput = int.Parse(input);
+            numbers.Add(userInput);
+        }
+
+        int max = numbers[0];
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+             if (number > max)
+            {
+                max = number;
+            }
+            
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max is: {max}");
+
+        
+
+
     }
 }
