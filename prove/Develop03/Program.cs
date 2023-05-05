d1135f012ec6719a12742b19d73045d496d963a2
reaganhsmith <reagansmith@byui.edu>
2023-05-05 14:35:53 -0700

    w03 prove

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,25 +6,31 @@ class Program
     static void Main(string[] args)
     {
 
+        string playAgain = "yes";
+        // string userInput = " ";
+        while(playAgain.ToLower() == "yes"){
+            // this gets info from the refernce class (book, chapter, start verse, end verse(this can be left empty))
+            Reference scriptureReference = new Reference("Proverbs", "3", "4", "5");
 
-        // this gets info from the refernce class (book, chapter, start verse, end verse(this can be left empty))
-        Reference scriptureReference = new Reference("Proverbs", "3", "4", "5");
+            Scripture scripture = new Scripture(scriptureReference, "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.");
+            
+            string userInput = " ";
 
-        Scripture scripture = new Scripture(scriptureReference, "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.");
-        
-        string userInput = " ";
-
-        
-        while (userInput != "QUIT" && scripture.HasWordsLeft() == true)
-        {
+            
+            while (userInput != "QUIT" && scripture.HasWordsLeft() == true)
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetScripture());
+                Console.WriteLine("Please press enter to continue or type QUIT to finish: ");
+                userInput = Console.ReadLine();
+                scripture.RemoveWords();
+            }
             Console.Clear();
             Console.WriteLine(scripture.GetScripture());
-            Console.WriteLine("Please press enter to continue or type QUIT to finish: ");
-            userInput = Console.ReadLine();
-            scripture.RemoveWords();
+            Console.WriteLine($"Thanks for memorizing {scriptureReference.GetReference()}!");
+            Console.WriteLine("Would you like to memorize it again (yes or no)?");
+            playAgain = Console.ReadLine();
         }
-        Console.Clear();
-        Console.WriteLine(scripture.GetScripture());
-        Console.WriteLine($"Thanks for memorizing {scriptureReference.GetReference()}! Have a nice day:)");
+        Console.WriteLine("Thanks, have a good day!");
     }
 }
