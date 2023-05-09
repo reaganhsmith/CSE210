a48d59580dbf19b8c51258021fd0c709b524a344
reaganhsmith <reagansmith@byui.edu>
2023-05-09 10:11:01 -0700

    Learning asisgnment w04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("");
+
+        Assignment a1 = new Assignment("Samuel Bennett", "multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Console.WriteLine("");
+
+        MathAssignment a2 =new MathAssignment("Reagan Smith", "Fractions", "7.0", "8-12");
+        Console.WriteLine(a2.GetHomeworkList());
+
+        Console.WriteLine(""); 
+
+        WritingAssignment a3 = new WritingAssignment("Braden Houser", "WWII", "The Second World War");
+        Console.WriteLine(a3.GetWitingInformation());
+        
+        Console.WriteLine("");
     }
 }
---
+++ prepare/Learning04/writingAssignment.cs
@@ -0,0 +1,17 @@
+
+public class WritingAssignment : Assignment{
+
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic){
+        _title = title;
+    }
+    public string GetWitingInformation()
+    {
+
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+
+}
