55f7a3017ff8be266510d0a4693593f522db84fc
reaganhsmith <reagansmith@byui.edu>
2023-05-09 17:00:41 -0700

    w04 prove assignment

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+
+        int userInput = 0;
+
+        List<string> menu = new List<string>{
+            "Menu Options",
+            "1. Start Breathing Activity",
+            "2. Start Reflection Activity",
+            "3. Start Listing Activity",
+            "4. Quit",
+            "What do you want to do: "
+        };
+
+        Breathing breathing = new Breathing();
+        Reflection reflection = new Reflection();
+        Listing listing = new Listing();
+
+
+        while(userInput != 4){
+            foreach(string menuItem in menu){
+                Console.WriteLine(menuItem);
+            }
+            string input = Console.ReadLine();
+            userInput = int.Parse(input);
+
+            if(userInput == 1)
+            {
+
+
+            }
+
+             if(userInput == 2)
+             {
+
+
+            }
+
+
+             if(userInput == 3)
+             {
+                
+            }
+        }
     }
 }
